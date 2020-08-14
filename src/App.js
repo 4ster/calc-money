ecdce9a64889903f597f934c7e1355dc38d9a6a1
Andrey Tokarev <abtokarev@gmail.com>
2020-08-14 18:24:28 +0300

    Home work 2 - totals

--- src/App.js
+++ src/App.js
@@ -1,10 +1,73 @@
-import React from 'react';
+import React, { Component, useLayoutEffect } from 'react';
 import Total from './components/total/Total';
 import Operation from './components/operation/Operation';
 import History from './components/history/History';
 
-function App() {
-  return (
+class App extends Component {
+  
+  state = {
+    transactions: [],
+    decription: '',
+    amount:'',
+    allPlus: 0,
+    allMinus: 0,
+    balance: 0
+  }
+
+  addTransaction = (add) => {
+
+    const transactions = [...this.state.transactions];
+    
+    const transaction = {
+      id: `cmr${(+new Date())}.toString(16)`,
+      description: this.state.description,
+      amount: +this.state.amount,
+      add,
+      }
+    
+    transactions.push(transaction);
+    
+
+    this.setState({ 
+      transactions,
+      description: '',
+      amount: ''},() => {
+        this.calcPlusMinus(true);
+        this.calcPlusMinus(false);
+    })
+  }
+
+  addAmount = (e) =>  {
+    this.setState({amount: e.target.value})
+  }
+
+  addDescription = (e) => {
+    this.setState({description: e.target.value})
+  }
+
+  getBalance = () => {
+    const balance = this.state.allPlus - this.state.allMinus;
+    this.setState({
+      balance
+    })
+  }
+
+  calcPlusMinus(add) {
+    const transactions = [...this.state.transactions];
+    
+    const reducer = (accumulator, currentValue) => currentValue.add===add ? accumulator + currentValue.amount : accumulator
+    const summ = transactions.reduce(reducer, 0)
+    
+    if (add){
+      this.setState({allPlus: summ})
+    }
+    else{
+      this.setState({allMinus: summ})
+    }
+  }
+  
+  render() {
+    return (
       <>
         <header>
         <h1>Кошелек</h1>
@@ -13,9 +76,19 @@ function App() {
 
       <main>
         <div className="container">
-          <Total/>
-          <Operation/>
-          <History/>
+          <Total
+          balance={this.state.balance}
+          allPlus={this.state.allPlus}
+          allMinus={this.state.allMinus}
+          />
+          <Operation
+          addTransaction={this.addTransaction}
+          addAmount = {this.addAmount}
+          addDescription={this.addDescription}
+          description = {this.state.description}
+          amount = {this.state.amount}/>
+          <History
+          transactions={this.state.transactions}/>
 
 
        </div>
@@ -23,7 +96,8 @@ function App() {
       </main>
       </>
       
-  );
+    );
+  }
 }
 
 export default App;
